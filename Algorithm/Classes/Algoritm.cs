0c4e8d954b79c21ee5bdb9f5a8372ef75c92cbd0
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-12-31 22:48:37 +0500

    Sum Two numbers And n Facktorial

---
+++ Algorithm/Classes/Algoritm.cs
@@ -0,0 +1,19 @@
+﻿namespace Algorithm.Classes
+{
+    public class Algoritm
+    {
+        public static int Sum(int a, int b)=>
+             a + b;
+        
+
+        public static int Factorial(int a)
+        {
+            int p = 1;
+            for (int i = 1; i <= a; i++)
+            {
+                p *= i;
+            }
+            return p;
+        }
+    }
+}
---
+++ Algorithm/Program.cs
@@ -0,0 +1,17 @@
+﻿//two numbers sum
+using Algorithm.Classes;
+
+Console.Write("a=");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b=");
+int b = int.Parse(Console.ReadLine());
+
+int s= Algoritm.Sum(a, b);
+
+Console.WriteLine($"{a}+{b}={s}");
+
+//factorial
+Console.Write("n=");
+int n = int.Parse(Console.ReadLine());
+int f = Algoritm.Factorial(n);
+Console.WriteLine($"{n}!={f}");
