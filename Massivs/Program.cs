a65e040e57a4029cf5a59b8b8ed9545e55e2500d
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-12-31 23:25:43 +0500

    Some Massiv Sum

---
+++ Massivs/Program.cs
@@ -0,0 +1,24 @@
+﻿//Sum massiv
+using Massivs.Classes;
+
+int[] massiv = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 ,9};
+
+int s = Massiv.SumMassiv(massiv);
+
+int kvs = Massiv.KvSum(massiv);
+
+int stub = Massiv.SumMassivTub(massiv);
+
+int sIndexToq = Massiv.SumIndexToq(massiv);
+
+int SumIndexKv = Massiv.SumIndexKv(massiv);
+
+Console.WriteLine($"S={s}");
+
+Console.WriteLine($"KvSum={kvs}");
+
+Console.WriteLine($"Toq={stub}");
+
+Console.WriteLine($"IndexToq={sIndexToq}");
+
+Console.WriteLine($"Sum Index KV={SumIndexKv}");
