e8a4239a8cdb176720215e01183be4182c2bfd87
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2025-01-04 23:02:06 +0500

    Sum N numbers

--- Numbers/Classes/Find.cs
+++ Numbers/Classes/Find.cs
@@ -142,6 +142,15 @@
             return n == 1 ? i : -1;
         }
 
+        public static int SummN(int n)
+        {
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += int.Parse(Console.ReadLine());
+            }
+            return sum;
+        }
 
     }
 }
